9923fa4cb65367b8e2747d8e4308cb80d2c7e9a0
Cameron Leverett <camrbo@gmail.com>
2021-04-06 14:31:16 -0700

    deleted some files I won't be useing

--- src/scratch.js
+++ src/scratch.js
@@ -1,6 +1,49 @@
 //randomArray(string, number)g
 //string options are ['string', 'integer', 'mix', 'object', 'array']
-const randomArray = require('../utils/arrays')
+const rad = require('../utils/arrays');
+const tool = require('../utils/tools');
+//builing array from scratch
+class MyArray {
+	constructor () {
+		this.length = 0;
+		this.data = {};
+	}
 
+	get (index) {
+		return this.data[index];
+	}
 
-console.log(randomArray('mix', 100))
+	push (item) {
+		this.data[this.length] = item;
+		this.length++;
+		return this.length;
+	}
+
+	pop () {
+		const lastItem = this.data[this.length - 1];
+		delete this.data[this.length - 1];
+		this.length--;
+		return lastItem;
+	}
+  
+  delete(index){
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for(let i = index; i < this.length -1 ;i++){
+      this.data[i] = this.data[i+1]
+    }
+    delete this.data[this.length-1]
+    this.length --
+  }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('!')
+console.log(newArray);
+newArray.delete(1)
+console.log(newArray);
